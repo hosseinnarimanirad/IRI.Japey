64c761f7a936fcf98f6d8010f6c747f4f7fe1fd1
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2018-04-23 22:55:44 +0430

    1397.02.03

--- IRI.Ket/IRI.Ket.Common/Web/MessageResult.cs
+++ IRI.Ket/IRI.Ket.Common/Web/MessageResult.cs
@@ -6,41 +6,41 @@ using System.Threading.Tasks;
 
 namespace IRI.Ket.Common.Web
 {
-    public class MessageResult<T>
-    {
-        public bool Succeed { get; set; }
+    //public class MessageResult<T>
+    //{
+    //    public bool Succeed { get; set; }
 
-        public T Result { get; set; }
+    //    public T Result { get; set; }
 
-        public string ErrorMessage { get; set; }
+    //    public string ErrorMessage { get; set; }
 
 
-        public MessageResult(T value)
-        {
-            this.Result = value;
-        }
+    //    public MessageResult(T value)
+    //    {
+    //        this.Result = value;
+    //    }
 
-        public MessageResult()
-        {
+    //    public MessageResult()
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
-    public static class MessageResult
-    {
-        public static MessageResult<string> CreateFailMessage(string error)
-        {
-            return new MessageResult<string>(string.Empty) { ErrorMessage = error, Succeed = false };
-        }
+    //public static class MessageResult
+    //{
+    //    public static MessageResult<string> CreateFailMessage(string error)
+    //    {
+    //        return new MessageResult<string>(string.Empty) { ErrorMessage = error, Succeed = false };
+    //    }
 
-        public static MessageResult<string> CreateOkMessage(string message)
-        {
-            return new MessageResult<string>(message) { Succeed = true };
-        }
+    //    public static MessageResult<string> CreateOkMessage(string message)
+    //    {
+    //        return new MessageResult<string>(message) { Succeed = true };
+    //    }
 
-        public static MessageResult<T> CreateOkMessage<T>(T result)
-        {
-            return new MessageResult<T>(result) { Succeed = true };
-        }
-    }
+    //    public static MessageResult<T> CreateOkMessage<T>(T result)
+    //    {
+    //        return new MessageResult<T>(result) { Succeed = true };
+    //    }
+    //}
 }
