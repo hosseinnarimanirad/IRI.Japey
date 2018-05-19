e6868616508f1eebb01a9ad8fb67320eb72154d8
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2018-05-20 00:23:47 +0430

    1397.02.30 UnitTest for GeoJson

---
+++ IRI.Sta/IRI.Sta.Common/Model/GeoJson/GeoJsonFeatureCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRI.Sta.Common.Model.GeoJson
+{
+
+    public class GeoJsonFeatureCollection
+    {
+        public string type { get; set; }
+        public int totalFeatures { get; set; }
+        public GeoJsonFeature[] features { get; set; }
+        public Crs crs { get; set; }
+    }
+
+    public class Crs
+    {
+        public string type { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Properties
+    {
+        public string name { get; set; }
+    }
+     
+
+}
