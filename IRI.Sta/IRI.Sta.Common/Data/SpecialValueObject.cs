4ffb468f8989a748721f6dc3e5428f89f9cbc942
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2021-04-26 23:34:34 +0430

    1400.02.06-ValueObject

---
+++ IRI.Sta/IRI.Sta.Common/Data/SpecialValueObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRI.Sta.Common.Data
+{
+    // 1400.02.06
+    // link: https://enterprisecraftsmanship.com/posts/value-object-better-implementation/
+    // the same as ValueObject but with automatic Equals and GetHashCode method implementation
+    public abstract class SpecialValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var valueObject = (SpecialValueObject)obj;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Aggregate(1, (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return current * 23 + (obj?.GetHashCode() ?? 0);
+                    }
+                });
+        }
+
+        public static bool operator ==(SpecialValueObject a, SpecialValueObject b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(SpecialValueObject a, SpecialValueObject b)
+        {
+            return !(a == b);
+        }
+    }
+
+
+}
