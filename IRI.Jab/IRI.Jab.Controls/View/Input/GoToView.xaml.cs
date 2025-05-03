5fc458f8ba95da968fea4a5f876e6f8c3744ed52
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2025-05-03 15:33:09 +0330

    [fix]- Goto tool input issue

--- IRI.Jab/IRI.Jab.Controls/View/Input/GoToView.xaml.cs
+++ IRI.Jab/IRI.Jab.Controls/View/Input/GoToView.xaml.cs
@@ -1,19 +1,5 @@
-﻿using IRI.Jab.Controls.Model.GoTo;
-using IRI.Jab.Controls.Presenter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IRI.Jab.Controls.View.Input
 {
@@ -23,6 +9,7 @@ namespace IRI.Jab.Controls.View.Input
     public partial class GoToView : UserControl
     {
         public Presenter.GoToPresenter Presenter { get => this.DataContext as Presenter.GoToPresenter; }
+
         public GoToView()
         {
             InitializeComponent();
