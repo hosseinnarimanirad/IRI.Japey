1e709fe9ce7eb51eaf10a7b799d9e6233cc7d342
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-05-25 14:46:31 +0330

    MapIndexesTest Passed test

--- IRI.Tst/IRI.Test.Main/Mapping/MapIndexesTest.cs
+++ IRI.Tst/IRI.Test.Main/Mapping/MapIndexesTest.cs
@@ -12,8 +12,18 @@ namespace IRI.Test.NetFrameworkTest.Common.Mapping
     {
         [Theory]
         [InlineData("5261", 46.0, 35.5, 46.5, 36.0)]
-        [InlineData("5372", 46.5, 36.0, 47.0, 36.5)]
-        [InlineData("5483", 47.0, 36.5, 47.5, 37.0)]
+        [InlineData("5372", 46.5, 41.0, 47.0, 41.5)]
+        [InlineData("5483", 47.0, 46.5, 47.5, 47.0)]
+        [InlineData("5050", 45.0, 30.0, 45.5, 30.5)]
+        [InlineData("5165", 45.5, 37.5, 46.0, 38.0)]
+        [InlineData("5242", 46.0, 26.0, 46.5, 26.5)]
+        [InlineData("5340", 46.5, 25.0, 47.0, 25.5)]
+        [InlineData("5430", 47.0, 20.0, 47.5, 20.5)]
+        [InlineData("5535", 47.5, 22.5, 48.0, 23.0)]
+        [InlineData("5641", 48.0, 25.5, 48.5, 26.0)]
+        [InlineData("5733", 48.5, 21.5, 49.0, 22.0)]
+        [InlineData("5844", 49.0, 27.0, 49.5, 27.5)]
+        [InlineData("5949", 49.5, 29.5, 50.0, 30.0)]
         public void Get100kGeodeticExtent_ValidIndex_ReturnsCorrectExtent(string index100k, double expectedXMin, double expectedYMin, double expectedXMax, double expectedYMax)
         {
             // Act
@@ -21,13 +31,12 @@ namespace IRI.Test.NetFrameworkTest.Common.Mapping
             Assert.NotNull(sheet);
 
             var extent = sheet.GeodeticExtent;
-            Assert.True(extent.HasValue, "GeodeticExtent is null");
 
             // Assert
-            Assert.Equal(expectedXMin, extent!.Value.XMin);
-            Assert.Equal(expectedYMin, extent.Value.YMin);
-            Assert.Equal(expectedXMax, extent.Value.XMax);
-            Assert.Equal(expectedYMax, extent.Value.YMax);
+            Assert.Equal(expectedXMin, extent.XMin);
+            Assert.Equal(expectedYMin, extent.YMin);
+            Assert.Equal(expectedXMax, extent.XMax);
+            Assert.Equal(expectedYMax, extent.YMax);
         }
     }
     }
