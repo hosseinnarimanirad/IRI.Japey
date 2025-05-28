98ae191ab11733921cf5da7fe5aff1877159dfe1
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-05-28 17:24:38 +0330

    Update Geometry_OgcTest.cs

--- IRI.Tst/IRI.Test.Main/Geometry/Geometry_OgcTest.cs
+++ IRI.Tst/IRI.Test.Main/Geometry/Geometry_OgcTest.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Data.SqlTypes; // For SqlBytes
 using Xunit;
 
-
 namespace IRI.Test.Main.TheGeometry
 {
     public class Geometry_OgcTest
@@ -17,7 +16,25 @@ namespace IRI.Test.Main.TheGeometry
             SqlServerTypes.Utilities.LoadNativeAssembliesv14();
         }
 
-        public static IEnumerable<object[]> OgcGeometryTestData =>
+        // Data source based on variables in original TestGeometryToWkb()
+        public static IEnumerable<object[]> WktTestDataFromOriginalFact =>
+            new List<object[]>
+            {
+                new object[] { "POINT(1 2)" },
+                new object[] { "MULTIPOINT((0 0), (0 3), (3 3), (3 0), (1 1), (9 9), (9 10), (10 9))" },
+                new object[] { "MULTIPOINT((2 3), (7 8))" },
+                new object[] { "LINESTRING(1 1, 2 0, 2 4, 3 3)" },
+                new object[] { "LINESTRING(4 4, 9 0)" },
+                new object[] { "MULTILINESTRING((1 1, 3 5), (-5 3, -8 -2))" },
+                new object[] { "POLYGON((0 0, 30 0, 30 30, 0 30, 0 0))" },
+                new object[] { "POLYGON((-20 -20, -20 20, 20 20, 20 -20, -20 -20), (10 0, 0 10, 0 -10, 10 0))" },
+                new object[] { "POLYGON((-20 -20, -20 20, 20 20, 20 -20, -20 -20), (10 0, 0 10, 0 -10, 10 0), (-10 0, -10 10, -15 0, -10 0))" },
+                new object[] { "MULTIPOLYGON(((0 0, 0 3, 3 3, 3 0, 0 0), (2 1, 1 2, 1 1, 2 1)), ((9 9, 9 10, 10 9, 9 9)))" },
+                new object[] { "MULTIPOLYGON(((0 0, 0 6, 6 6, 6 0, 0 0), (5 5, 5 1, 1 1, 1 5, 5 5)), ((2 4, 2 2, 4 2, 4 4, 2 4),(3.5 3.5, 2.5 3.5, 2.5 2.5, 3.5 2.5, 3.5 3.5)))" }
+            };
+
+        // Data source based on InlineData in original TestWkbToGeometry()
+        public static IEnumerable<object[]> WktTestDataFromOriginalTheory =>
             new List<object[]>
             {
                 new object[] { "POINT(1 2)" },
@@ -33,28 +50,10 @@ namespace IRI.Test.Main.TheGeometry
                 new object[] { "MULTIPOLYGON(((0 0, 0 6, 6 6, 6 0, 0 0), (1 5, 1 1, 5 1, 5 5, 1 5)), ((4 4, 4 2, 2 2, 2 4, 4 4),(3.5 3.5, 2.5 3.5, 2.5 2.5, 3.5 2.5, 3.5 3.5)))" }
             };
 
-        [Theory]
-        [MemberData(nameof(OgcGeometryTestData))]
-        public void TestWktToWkbToWkt_Roundtrip_UsingIriStaticFromWkb(string originalWkt)
-        {
-            // Arrange
-            var initialGeometry = WktParser.Parse(originalWkt);
-            // اگر initialGeometry اینجا null باشد یا AsWkb() مقدار null یا خالی برگرداند،
-            // تست در خط بعدی یا خطوط بعد با NullReferenceException یا ArgumentNullException شکست می‌خورد.
-            var wkb = initialGeometry.AsWkb();
-
-            // Act
-            var geometryFromWkb = Geometry<Point>.FromWkb(wkb, 0);
-            // اگر geometryFromWkb اینجا null باشد، تست در خط بعدی با NullReferenceException شکست می‌خورد.
-            var finalWkt = geometryFromWkb.AsWkt();
-
-            // Assert
-            Assert.Equal(initialGeometry.AsWkt(), finalWkt);
-        }
 
         [Theory]
-        [MemberData(nameof(OgcGeometryTestData))]
-        public void TestWktToWkbToWkt_Roundtrip_UsingSqlGeometryFromWkb(string originalWkt)
+        [MemberData(nameof(WktTestDataFromOriginalFact))] // Uses data from the original Fact's variables
+        public void TestWkbRoundtrip_ViaSqlGeometry(string originalWkt)
         {
             // Arrange
             var initialGeometry = WktParser.Parse(originalWkt);
@@ -68,32 +67,63 @@ namespace IRI.Test.Main.TheGeometry
             Assert.Equal(initialGeometry.AsWkt(), finalWkt);
         }
 
+        [Theory]
+        [MemberData(nameof(WktTestDataFromOriginalTheory))] // Uses data from the original Theory's InlineData
+        public void TestWkbRoundtrip_ViaIriStaticMethod(string originalWkt)
+        {
+            // Arrange
+            var initialGeometry = WktParser.Parse(originalWkt);
+            var wkb = initialGeometry.AsWkb();
+
+            // Act
+            var geometryFromWkb = Geometry<Point>.FromWkb(wkb, 0);
+            var finalWkt = geometryFromWkb.AsWkt();
+
+            // Assert
+            Assert.Equal(initialGeometry.AsWkt(), finalWkt);
+        }
+
+        // Data source for WKT serialization, based on string variables in original TestWktToGeometry()
         public static IEnumerable<object[]> WktSerializationTestData()
         {
-            foreach (var itemArray in OgcGeometryTestData)
+            // These WKT strings are identical to WktTestDataFromOriginalFact in this case
+            var wktStrings = new List<string>
             {
-                var inputWkt = (string)itemArray[0];
-                var expectedWkt = inputWkt.Contains("POINT") && !inputWkt.Contains("MULTIPOINT") ? inputWkt : inputWkt.Replace(", ", ",");
+                "POINT(1 2)",
+                "MULTIPOINT((0 0), (0 3), (3 3), (3 0), (1 1), (9 9), (9 10), (10 9))",
+                "MULTIPOINT((2 3), (7 8))",
+                "LINESTRING(1 1, 2 0, 2 4, 3 3)",
+                "LINESTRING(4 4, 9 0)",
+                "MULTILINESTRING((1 1, 3 5), (-5 3, -8 -2))",
+                "POLYGON((0 0, 30 0, 30 30, 0 30, 0 0))",
+                "POLYGON((-20 -20, -20 20, 20 20, 20 -20, -20 -20), (10 0, 0 10, 0 -10, 10 0))",
+                "POLYGON((-20 -20, -20 20, 20 20, 20 -20, -20 -20), (10 0, 0 10, 0 -10, 10 0), (-10 0, -10 10, -15 0, -10 0))",
+                "MULTIPOLYGON(((0 0, 0 3, 3 3, 3 0, 0 0), (2 1, 1 2, 1 1, 2 1)), ((9 9, 9 10, 10 9, 9 9)))",
+                "MULTIPOLYGON(((0 0, 0 6, 6 6, 6 0, 0 0), (5 5, 5 1, 1 1, 1 5, 5 5)), ((2 4, 2 2, 4 2, 4 4, 2 4),(3.5 3.5, 2.5 3.5, 2.5 2.5, 3.5 2.5, 3.5 3.5)))"
+            };
+
+            foreach (var inputWkt in wktStrings)
+            {
+                // Logic from original TestWktToGeometry for expected output
+                var expectedWkt = (inputWkt.StartsWith("POINT(") && !inputWkt.StartsWith("MULTIPOINT("))
+                                    ? inputWkt
+                                    : inputWkt.Replace(", ", ",");
                 yield return new object[] { inputWkt, expectedWkt };
             }
         }
 
         [Theory]
         [MemberData(nameof(WktSerializationTestData))]
-        public void TestWkt_SerializationAndParsing_Roundtrip(string inputWkt, string expectedCanonicalWkt)
+        public void TestWktSerialization_ParseThenAsWkt(string inputWkt, string expectedSerializedWkt)
         {
             // Arrange
             var parsedGeometry = WktParser.Parse(inputWkt);
 
             // Act
-            var serializedWkt = parsedGeometry.AsWkt();
+            var actualSerializedWkt = parsedGeometry.AsWkt();
 
             // Assert
-            Assert.Equal(expectedCanonicalWkt, serializedWkt);
-
-            var reparsedGeometry = WktParser.Parse(serializedWkt);
-            // اگر reparsedGeometry اینجا null باشد، تست در خط بعدی با NullReferenceException شکست می‌خورد.
-            Assert.Equal(expectedCanonicalWkt, reparsedGeometry.AsWkt());
+            Assert.Equal(expectedSerializedWkt, actualSerializedWkt);
         }
     }
 }
