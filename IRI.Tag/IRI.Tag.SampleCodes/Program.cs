d326ffb287f2e45640279508bf6e453ce08c827e
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2025-06-23 10:31:28 +0330

    [chore]-sample geojson vs shapefile

--- IRI.Tag/IRI.Tag.SampleCodes/Program.cs
+++ IRI.Tag/IRI.Tag.SampleCodes/Program.cs
@@ -1,2 +1,6 @@
 ﻿
-IRI.Tag.SampleCodes.Geodesy.Geodesy_DegreeDistance.DegreeToEarthDistance(0);
+//IRI.Tag.SampleCodes.Geodesy.Geodesy_DegreeDistance.DegreeToEarthDistance(0);
+
+IRI.Tag.SampleCodes.SpatialFormats.GeoJsonVsShapefile.CompareGeoJsonVsShapefile(
+    @"E:\Programming\100.IRI.Japey\IRI.Tst\IRI.Test.Main\Assets\GeoJsonSamples\networkblock.json",
+    @"C:\Users\Hossein\Desktop\New folder (4)");
