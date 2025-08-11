2999f80e5685ea6a781dbe0444c07172437abdd3
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2025-08-11 22:06:32 +0330

    refactor: move research projects

---
+++ research/IRI.Maptor.Res.FastSimplification/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace IRI.Maptor.Res.FastSimplification;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        await SimplificationHelper.GeneralTest();
+    }
+}
