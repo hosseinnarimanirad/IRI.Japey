32a40b6a7c33a4fb50b4341904b74a1c8af40642
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2025-08-14 20:54:40 +0330

    test(geometry): ground area/length

--- tests/IRI.Maptor.Tst.Main/Benchmarking/SqlSpatialToBitmap.cs
+++ tests/IRI.Maptor.Tst.Main/Benchmarking/SqlSpatialToBitmap.cs
@@ -7,182 +7,181 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.IO;
 
-namespace IRI.Benchmarking.Benchmarking
+namespace IRI.Maptor.Tst.Main.Benchmarking;
+
+static class SqlSpatialToBitmap
 {
-    static class SqlSpatialToBitmap
+    public static void Go()
     {
-        public static void Go()
+        DateTime t0 = DateTime.Now;
+
+        System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+
+
+        //*********************************************************
+        watch.Start();                                          //*
+        DateTime t2 = DateTime.Now;                             //*
+        //*********************************************************
+        //
+        string connectionString = @"server = YA-MORTAZA\MSSQLSERVER2012; integrated security = true;user id=sa; password=sa123456; database = TestSpatialDatabase";
+        //
+        SqlConnection connection = new SqlConnection(connectionString);
+        //
+        SqlCommand command = new SqlCommand("SELECT Geo FROM TehranBlocksMercator", connection);
+        //
+        connection.Open();
+        //
+        List<Microsoft.SqlServer.Types.SqlGeometry> geometries = new List<Microsoft.SqlServer.Types.SqlGeometry>();
+        //
+        SqlDataReader reader = command.ExecuteReader();
+        //
+        while (reader.Read())
         {
-            DateTime t0 = DateTime.Now;
-
-            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
-
-
-            //*********************************************************
-            watch.Start();                                          //*
-            DateTime t2 = DateTime.Now;                             //*
-            //*********************************************************
-            //
-            string connectionString = @"server = YA-MORTAZA\MSSQLSERVER2012; integrated security = true;user id=sa; password=sa123456; database = TestSpatialDatabase";
-            //
-            SqlConnection connection = new SqlConnection(connectionString);
-            //
-            SqlCommand command = new SqlCommand("SELECT Geo FROM TehranBlocksMercator", connection);
-            //
-            connection.Open();
-            //
-            List<Microsoft.SqlServer.Types.SqlGeometry> geometries = new List<Microsoft.SqlServer.Types.SqlGeometry>();
-            //
-            SqlDataReader reader = command.ExecuteReader();
-            //
-            while (reader.Read())
+            Microsoft.SqlServer.Types.SqlGeometry sqlGeometry = new Microsoft.SqlServer.Types.SqlGeometry();
+            System.Data.SqlTypes.SqlBytes bytes = reader.GetSqlBytes(0);
+            using (MemoryStream stream = new MemoryStream(bytes.Buffer))
             {
-                Microsoft.SqlServer.Types.SqlGeometry sqlGeometry = new Microsoft.SqlServer.Types.SqlGeometry();
-                System.Data.SqlTypes.SqlBytes bytes = reader.GetSqlBytes(0);
-                using (MemoryStream stream = new MemoryStream(bytes.Buffer))
+                using (BinaryReader binaryReader = new BinaryReader(stream))
                 {
-                    using (BinaryReader binaryReader = new BinaryReader(stream))
-                    {
-                        sqlGeometry.Read(binaryReader);
-                    }
+                    sqlGeometry.Read(binaryReader);
                 }
-                geometries.Add(sqlGeometry.MakeValid().Reduce(10));
             }
-            //
-            //*********************************************************
-            watch.Stop();                                           //*
-            TimeSpan dt2 = DateTime.Now - t2;                       //*
-            var shapesToSqlSpatial = watch.ElapsedMilliseconds;     //*
-            watch.Reset();                                          //*
-            //*********************************************************
-            //*********************************************************
-            //*********************************************************
+            geometries.Add(sqlGeometry.MakeValid().Reduce(10));
+        }
+        //
+        //*********************************************************
+        watch.Stop();                                           //*
+        TimeSpan dt2 = DateTime.Now - t2;                       //*
+        var shapesToSqlSpatial = watch.ElapsedMilliseconds;     //*
+        watch.Reset();                                          //*
+        //*********************************************************
+        //*********************************************************
+        //*********************************************************
 
 
 
-            //*********************************************************
-            //*********************************************************
-            watch.Start();                                          //*
-            DateTime t3 = DateTime.Now;                             //*
-            //*********************************************************
-            //
-            //ScaleTransform transform = new ScaleTransform(+1, -1);
-            //StreamGeometry geo =
-            //    IRI.Maptor.Jab.Common.Convertor.StreamGeometryRenderer.ParseSqlGeometry(geometries, p => transform.Transform(p));
-            //
-            //*********************************************************
-            watch.Stop();                                           //*
-            TimeSpan dt3 = DateTime.Now - t3;                       //*
-            var sqlSpatialToStreamGeometry = watch.ElapsedMilliseconds;//*
-            watch.Reset();                                          //*
-            //*********************************************************
-            //*********************************************************
-            //*********************************************************
+        //*********************************************************
+        //*********************************************************
+        watch.Start();                                          //*
+        DateTime t3 = DateTime.Now;                             //*
+        //*********************************************************
+        //
+        //ScaleTransform transform = new ScaleTransform(+1, -1);
+        //StreamGeometry geo =
+        //    IRI.Maptor.Jab.Common.Convertor.StreamGeometryRenderer.ParseSqlGeometry(geometries, p => transform.Transform(p));
+        //
+        //*********************************************************
+        watch.Stop();                                           //*
+        TimeSpan dt3 = DateTime.Now - t3;                       //*
+        var sqlSpatialToStreamGeometry = watch.ElapsedMilliseconds;//*
+        watch.Reset();                                          //*
+        //*********************************************************
+        //*********************************************************
+        //*********************************************************
 
 
 
-            //*********************************************************
-            watch.Start();                                          //*
-            DateTime t4 = DateTime.Now;                             //*
-            //*********************************************************
-            //
-            DrawingGroup drawing = new DrawingGroup();
-            //
-            //using (DrawingContext dc = drawing.Open())
-            //{
-            //    dc.DrawGeometry(new SolidColorBrush(Colors.Orange), new Pen(new SolidColorBrush(Colors.Black), 5), geo);
-            //}
-            //
-            //*********************************************************
-            watch.Stop();                                           //*
-            TimeSpan dt4 = DateTime.Now - t4;                       //*
-            var streamGeometryToDrawing = watch.ElapsedMilliseconds;//*
-            watch.Reset();                                          //*
-            //*********************************************************
-            //*********************************************************
-            //*********************************************************
+        //*********************************************************
+        watch.Start();                                          //*
+        DateTime t4 = DateTime.Now;                             //*
+        //*********************************************************
+        //
+        DrawingGroup drawing = new DrawingGroup();
+        //
+        //using (DrawingContext dc = drawing.Open())
+        //{
+        //    dc.DrawGeometry(new SolidColorBrush(Colors.Orange), new Pen(new SolidColorBrush(Colors.Black), 5), geo);
+        //}
+        //
+        //*********************************************************
+        watch.Stop();                                           //*
+        TimeSpan dt4 = DateTime.Now - t4;                       //*
+        var streamGeometryToDrawing = watch.ElapsedMilliseconds;//*
+        watch.Reset();                                          //*
+        //*********************************************************
+        //*********************************************************
+        //*********************************************************
 
 
 
-            //*********************************************************
-            watch.Start();                                          //*
-            DateTime t5 = DateTime.Now;                             //*
-            //*********************************************************
-            //
-            var drawingVisual = new DrawingVisual();
-            //
-            double scale = 1 / 25.0;
-            //
-            using (var drawingContext = drawingVisual.RenderOpen())
-            {
-                drawingContext.PushTransform(new ScaleTransform(scale, scale));
+        //*********************************************************
+        watch.Start();                                          //*
+        DateTime t5 = DateTime.Now;                             //*
+        //*********************************************************
+        //
+        var drawingVisual = new DrawingVisual();
+        //
+        double scale = 1 / 25.0;
+        //
+        using (var drawingContext = drawingVisual.RenderOpen())
+        {
+            drawingContext.PushTransform(new ScaleTransform(scale, scale));
 
-                drawingContext.PushTransform(new TranslateTransform(-drawing.Bounds.X, -drawing.Bounds.Y));
+            drawingContext.PushTransform(new TranslateTransform(-drawing.Bounds.X, -drawing.Bounds.Y));
 
-                drawingContext.DrawDrawing(drawing);
-
-            }
-            //
-            //*********************************************************
-            watch.Stop();                                           //*
-            TimeSpan dt5 = DateTime.Now - t5;                       //*
-            var drawingToDrawingVisual = watch.ElapsedMilliseconds; //*
-            watch.Reset();                                          //*
-            //*********************************************************
-            //*********************************************************
-            //*********************************************************
-
-
-
-            //*********************************************************
-            watch.Start();                                          //*
-            DateTime t6 = DateTime.Now;                             //*
-            //*********************************************************
-            //
-            var width = drawing.Bounds.Width * scale;
-            //
-            var height = drawing.Bounds.Height * scale;
-            //
-            var bitmap = new RenderTargetBitmap((int)width, (int)height, 96, 96, PixelFormats.Pbgra32);
-            //          
-            bitmap.Render(drawingVisual);
-            //
-            var encoder = new PngBitmapEncoder();
-            //
-            encoder.Frames.Add(BitmapFrame.Create(bitmap));
-            //
-            //*********************************************************
-            watch.Stop();                                           //*
-            TimeSpan dt6 = DateTime.Now - t6;                       //*
-            var VisualToBitmap = watch.ElapsedMilliseconds; //*
-            watch.Reset();                                          //*
-            //*********************************************************
-            //*********************************************************
-            //*********************************************************
-
-
-            //*********************************************************
-            watch.Start();                                          //*
-            DateTime t7 = DateTime.Now;                             //*
-            //*********************************************************
-            //
-            using (var stream = new FileStream("D:\\result.Reduce10Border.bmp", FileMode.Create))
-            {
-                encoder.Save(stream);
-            }
-            //
-            //*********************************************************
-            watch.Stop();                                           //*
-            TimeSpan dt7 = DateTime.Now - t7;                       //*
-            var saveImage = watch.ElapsedMilliseconds; //*
-            watch.Reset();                                          //*
-            //*********************************************************
-            //*********************************************************
-            //*********************************************************
-
-            TimeSpan dt = DateTime.Now - t0;
-
+            drawingContext.DrawDrawing(drawing);
 
         }
+        //
+        //*********************************************************
+        watch.Stop();                                           //*
+        TimeSpan dt5 = DateTime.Now - t5;                       //*
+        var drawingToDrawingVisual = watch.ElapsedMilliseconds; //*
+        watch.Reset();                                          //*
+        //*********************************************************
+        //*********************************************************
+        //*********************************************************
+
+
+
+        //*********************************************************
+        watch.Start();                                          //*
+        DateTime t6 = DateTime.Now;                             //*
+        //*********************************************************
+        //
+        var width = drawing.Bounds.Width * scale;
+        //
+        var height = drawing.Bounds.Height * scale;
+        //
+        var bitmap = new RenderTargetBitmap((int)width, (int)height, 96, 96, PixelFormats.Pbgra32);
+        //          
+        bitmap.Render(drawingVisual);
+        //
+        var encoder = new PngBitmapEncoder();
+        //
+        encoder.Frames.Add(BitmapFrame.Create(bitmap));
+        //
+        //*********************************************************
+        watch.Stop();                                           //*
+        TimeSpan dt6 = DateTime.Now - t6;                       //*
+        var VisualToBitmap = watch.ElapsedMilliseconds; //*
+        watch.Reset();                                          //*
+        //*********************************************************
+        //*********************************************************
+        //*********************************************************
+
+
+        //*********************************************************
+        watch.Start();                                          //*
+        DateTime t7 = DateTime.Now;                             //*
+        //*********************************************************
+        //
+        using (var stream = new FileStream("D:\\result.Reduce10Border.bmp", FileMode.Create))
+        {
+            encoder.Save(stream);
+        }
+        //
+        //*********************************************************
+        watch.Stop();                                           //*
+        TimeSpan dt7 = DateTime.Now - t7;                       //*
+        var saveImage = watch.ElapsedMilliseconds; //*
+        watch.Reset();                                          //*
+        //*********************************************************
+        //*********************************************************
+        //*********************************************************
+
+        TimeSpan dt = DateTime.Now - t0;
+
+
     }
 }
---
+++ tests/IRI.Maptor.Tst.Main/Geometry/EllipsoidalAreaTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.SqlServer.Types;
+using System.Data.SqlTypes;
+using IRI.Maptor.Sta.Spatial.Primitives;
+using IRI.Maptor.Extensions;
+
+namespace IRI.Maptor.Tst.Main.Geometry;
+
+public class EllipsoidalAreaTests
+{
+    // EPSG:4326 (WGS84)
+    private const int Wgs84Srid = 4326;
+
+    [Theory]
+    [InlineData("POLYGON((0 0,0 0.01,0.01 0.01,0.01 0,0 0))")]       
+    [InlineData("POLYGON((10 10,10 10.01,10.01 10.01,10.01 10,10 10))")] 
+    [InlineData("POLYGON((30 30,30 30.02,30.02 30.02,30.02 30,30 30))")] 
+    [InlineData("POLYGON((0 0,0 0.005,0.005 0.005,0.005 0,0 0))")]  
+    [InlineData("POLYGON((-5 -5,-5 -4.99,-4.99 -4.99,-4.99 -5,-5 -5))")]  
+    [InlineData("POLYGON((0 0,0.05 0,0.05 0.05,0 0.05,0 0),(0.01 0.02,0.02 0.02,0.02 0.01,0.01 0.01,0.01 0.02))")]
+    [InlineData("MULTIPOLYGON(((0.02 0.03,0.02 0.02,0.03 0.02,0.03 0.03,0.02 0.03)),((0.00 0.01,0.00 0.00,0.01 0.00,0.01 0.01,0.00 0.01)))")]
+    [InlineData("MULTIPOLYGON(((1.00 1.03,1.00 1.00,1.03 1.00,1.03 1.03,1.00 1.03)),((0.00 0.04,0.00 0.00,0.04 0.00,0.04 0.04,0.00 0.04),(0.01 0.03,0.03 0.03,0.03 0.01,0.01 0.01,0.01 0.03)))")]
+    [InlineData("MULTIPOLYGON(((50.0000 60.0523,50.0000 60.0000,50.0523 60.0000,50.0523 60.0523,50.0000 60.0523)),((30.0000 40.0805,30.0000 40.0000,30.0817 40.0000,30.0817 40.0805,30.0000 40.0805), (30.0314 40.0317,30.0716 40.0317,30.0716 40.0118,30.0314 40.0118,30.0314 40.0317), (30.0215 40.0711,30.0613 40.0711,30.0613 40.0513,30.0215 40.0513,30.0215 40.0711)),((10.0514 20.0814,10.0514 20.0711,10.0611 20.0711,10.0611 20.0814,10.0514 20.0814)),((10.0000 20.1012,10.0000 20.0000,10.1025 20.0000,10.1025 20.1012,10.0000 20.1012), (10.0415 20.0412,10.0812 20.0412,10.0812 20.0214,10.0415 20.0214,10.0415 20.0412), (10.0314 20.0814,10.0514 20.0814,10.0514 20.0912,10.0611 20.0912,10.0611 20.0814,10.0713 20.0814,10.0713 20.0613,10.0314 20.0613,10.0314 20.0814)))")]
+    public void Compare_CalculateSphereArea_With_SqlGeography(string wkt)
+    {
+        // Arrange
+        var sqlGeo = SqlGeography.STGeomFromText(new SqlChars(wkt), Wgs84Srid);
+        if (sqlGeo.EnvelopeAngle() >= 180)
+        {
+            sqlGeo = sqlGeo.ReorientObject();
+        }
+
+        double expected = sqlGeo.STArea().Value; // SQL Server area in m²
+         
+        var geometry = Geometry<IRI.Maptor.Sta.Common.Primitives.Point>.FromWkt(wkt, Wgs84Srid);
+        var newGeo = geometry.Project(new Sta.SpatialReferenceSystem.MapProjections.CylindricalEqualArea());
+
+
+        // Act 
+        double actual = newGeo.EuclideanArea; 
+         
+        // Assert
+        double diff = Math.Abs(expected - actual);
+        double relativeError = diff / expected;
+         
+        Assert.True(relativeError < 0.0001,
+            $"Area mismatch: WKT={wkt}, Expected={expected}, Actual={actual}, Diff={diff}, RelErr={relativeError}");
+
+        // does not pass for large polygons
+        //Assert.Equal(expected, actual, 5);
+    }
+}
